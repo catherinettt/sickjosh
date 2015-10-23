eff66c6681fe9bce94a20436469e4d9e0971ccd3
Toby Sullivan <tobyjsullivan@gmail.com>
2015-10-23 00:06:07 -0700

    #3: Add game status placeholder to admin screen and refactor player list a bit

--- public/src/components/admin.jsx
+++ public/src/components/admin.jsx
@@ -9,6 +9,87 @@ require('./admin.less')
 var ws = require('../ws-utils');
 var _ = require('underscore');
 
+// TODO: Remove hardcoded state and players
+var gameState = "active";
+var players = [
+  {
+    playerName: "toby.sullivan",
+    zombie: false,
+    ready: true
+  },
+  {
+    playerName: "other.guy",
+    zombie: false,
+    ready: false
+  },
+  {
+    playerName: "zombie.person",
+    zombie: true,
+    ready: true
+  }
+];
+
+var GameStatus = React.createClass({
+  render: function() {
+    var statusBar = "";
+    if (this.props.status.state == "lobby") {
+      statusBar = (
+        <div className='-status-lobby'>
+          {this.props.status.readyPlayers}/{this.props.status.totalPlayers} Players Ready
+          &nbsp;
+          <button className='btn'>Start Game</button>
+        </div>
+      )
+    } else if (this.props.status.state == "active") {
+      statusBar = (
+        <div className='-status-active'>
+          Game Active <button className='btn'>End Game</button>
+        </div>
+      )
+    };
+    return(
+      <div className='-game-status'>
+        <div className='row'>
+          <div className='col-xs-4'>
+            {statusBar}
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var PlayerListItem = React.createClass({
+  render: function() {
+    var icon = '';
+    if (this.props.gameState == "lobby") {
+      icon = this.props.player.ready ? 'glyphicon glyphicon-ok' : '';
+    } else if (this.props.gameState == "active") {
+      icon = this.props.player.zombie ? 'glyphicon glyphicon-tint' : 'glyphicon glyphicon-user';
+    }
+
+    return (
+      <div className='col-xs-4 -player'>
+        <span className={icon}></span> <span>{this.props.player.playerName}</span>
+      </div>
+    );
+  }
+});
+
+var PlayerList = React.createClass({
+  render: function() {
+    var playerListItems = _.map(this.props.players, (player) => {
+      return (<PlayerListItem key={player.playerName} player={player} gameState={this.props.gameState}></PlayerListItem>)
+    });
+    return (
+      <div className='-players'>
+        <h4>Players <small className='pull-right'><span className='glyphicon glyphicon-tint'></span>: infected</small> </h4>
+        <div className='row'>{playerListItems}</div>
+      </div>
+    );
+  }
+});
+
 class Admin extends React.Component {
   constructor() {
     super();
@@ -41,29 +122,16 @@ class Admin extends React.Component {
     }
   }
 
-  renderPlayers() {
-    if (_.size(this.state.registeredPlayers)) {
-      return _.map(this.state.registeredPlayers, (player) => {
-        var zombie = player.zombie ? 'glyphicon glyphicon-tint' : 'glyphicon glyphicon-user';
-        return (
-          <div className='col-xs-4 -player' key={player.playerName}> 
-             <span className={zombie}></span> <span>{player.playerName}</span>
-          </div>
-        )
-      })
-    }
-  }
-
   renderSafezones() {
     if (this.state.safezones.length) {
       return this.state.safezones.map((zone) => {
         return (
-          <div className='row' key={zone.id}> 
+          <div className='row' key={zone.id}>
             <div className='col-xs-8'>
               {zone.get('map').get('name')} > {zone.get('name')} (Max: {zone.get('maxAllowance')})
             </div>
             <div className='col-xs-4'>
-              <button className='btn btn-xs btn-default pull-right'>Activate</button> 
+              <button className='btn btn-xs btn-default pull-right'>Activate</button>
             </div>
           </div>
         )
@@ -72,14 +140,18 @@ class Admin extends React.Component {
   }
 
   render() {
+    var totalPlayers = _.size(players);
+    var readyPlayers = _.size(_.filter(players, function(player) { return player.ready; }));
+
+    var gameStatus = {
+      state: gameState,
+      readyPlayers: readyPlayers,
+      totalPlayers: totalPlayers
+    }
     return (
-      <div className='js-admin container-fluid'> 
-        <div className='-players'>
-          <h4>Players <small className='pull-right'><span className='glyphicon glyphicon-tint'></span>: infected</small> </h4>
-          <div className='row'> 
-            {this.renderPlayers()}
-          </div>
-        </div>
+      <div className='js-admin container-fluid'>
+        <GameStatus status={gameStatus}></GameStatus>
+        <PlayerList players={players} gameState={gameStatus.state}></PlayerList>
         <hr />
         <div className='-safezones'>
           <h4>Safezones</h4>
